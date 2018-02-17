02d72af2ac3b0657c9710bc05f89c1907942dac6
Satit Rianpit <rianpit@gmail.com>
2018-02-17 15:35:08 +0700

    init db connection

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,16 @@
+require('dotenv').config();
+
 import * as express from 'express';
 import * as path from 'path';
 import * as favicon from 'serve-favicon';
 import * as logger from 'morgan';
 import * as cookieParser from 'cookie-parser';
 import * as bodyParser from 'body-parser';
+import Knex = require('knex');
+import { MySqlConnectionConfig } from 'knex';
+import * as cors from 'cors';
+const protect = require('@risingstack/protect');
+
 import index from './routes/index';
 
 const app: express.Express = express();
@@ -19,9 +26,48 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, '../public')));
+app.use(cors());
 
+app.use(protect.express.xss({
+  body: true,
+  loggerFunction: console.error
+}));
+
+// Database connection string
+let dbConnection: MySqlConnectionConfig = {
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  multipleStatements: true
+}
+
+// Database connection
+app.use((req, res, next) => {
+  req.db = Knex({
+    client: 'mysql',
+    connection: dbConnection,
+    pool: {
+      min: 0,
+      max: 7,
+      afterCreate: (conn, done) => {
+        conn.query(`SET NAMES ${process.env.DB_ENCODING}`, (err) => {
+          done(err, conn);
+        });
+      }
+    },
+    debug: process.env.SQL_DEBUG || true,
+    acquireConnectionTimeout: 5000
+  });
+
+  next();
+});
+
+// Routing
 app.use('/', index);
 
+//catch 404 and forward to error handler
 //catch 404 and forward to error handler
 app.use((req, res, next) => {
   var err = new Error('Not Found');
@@ -29,30 +75,18 @@ app.use((req, res, next) => {
   next(err);
 });
 
-//error handlers
-
-//development error handler
-//will print stacktrace
-if (process.env.NODE_ENV === 'development') {
-  app.use((err: Error, req, res, next) => {
-    res.status(err['status'] || 500);
-    res.render('error', {
-      title: 'error',
-      message: err.message,
-      error: err
-    });
-  });
-}
-
-//production error handler
-// no stacktrace leaked to user
 app.use((err: Error, req, res, next) => {
-  res.status(err['status'] || 500);
-  res.render('error', {
-    title: 'error',
-    message: err.message,
-    error: {}
-  });
+  console.log(err);
+  let errorMessage;
+  switch (err['code']) {
+    case 'ER_DUP_ENTRY':
+      errorMessage = 'ข้อมูลซ้ำ';
+      break;
+    default:
+      errorMessage = err;
+      res.status(err['status'] || 500);
+  }
+  res.send({ ok: false, error: errorMessage });
 });
 
 export default app;
